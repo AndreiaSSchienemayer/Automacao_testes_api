0bd4d5eb93a42961224cbe2574029f744a146f15
Andréia <40999082+AndreiaSchienemayer@users.noreply.github.com>
2025-08-22 21:13:59 -0300

    Adicionando teste de API via HTTP e execução na pipeline

---
+++ test/external/transferExternal.test.js
@@ -0,0 +1,23 @@
+//Biblioteca
+const request = require('supertest');
+const {expect}=require('chai');
+
+
+//Testes
+describe('Transfer Controller', () => {
+    describe('POST / transfer', () => {
+        it ('Quando informo remetente e destinatário inexistente recebo 400', async () => {
+            const resposta = await request('http://localhost:3000')
+                .post('/transfer')
+                .send({
+                    from:"Andreia",
+                    to:"Arthur",
+                    amount:100
+                });
+
+            expect(resposta.status).to.equal(400);
+            console.log(resposta.body);
+            expect(resposta.body).to.have.property('error','Usuário não encontrado.')
+        });
+    });
+});
